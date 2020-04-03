9257850206d4373405e2fca82f5eb1d4ab8b3084
Shinung <bebop88c@gmail.com>
2020-04-03 22:14:38 +0900

    Updates

---
+++ precache-manifest.b80f3c20a031f7b70e5db0dca30bd8a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d414f4d8357d46f356e2a121160f625",
+    "url": "/movie-app-nomad/index.html"
+  },
+  {
+    "revision": "41e318c28d1b6b46f2fe",
+    "url": "/movie-app-nomad/static/css/main.547f4d3f.chunk.css"
+  },
+  {
+    "revision": "2628140d32e7396ff1a9",
+    "url": "/movie-app-nomad/static/js/2.8a3adbee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app-nomad/static/js/2.8a3adbee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41e318c28d1b6b46f2fe",
+    "url": "/movie-app-nomad/static/js/main.6c46a31b.chunk.js"
+  },
+  {
+    "revision": "ad6adedf9841831879e3",
+    "url": "/movie-app-nomad/static/js/runtime-main.36c9ee11.js"
+  }
+]);
